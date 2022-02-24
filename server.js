1e8ed825c6c9c87f9a838258c2ecad78fcce8c7a
Chandramouli B <chandramoulib@hydkspl.com>
2022-02-25 00:05:04 +0530

    changes

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path');
 const app = express();
 const port = 3000
 
-app.use(express.static(__dirname + '/dist/EcomPortal'));
+app.use(express.static(__dirname + '/dist/ecom-portal'));
 
 app.use('/*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/dist/EcomPortal/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/ecom-portal/index.html'));
 });
 
 app.listen(process.env.PORT || port, () => console.log('application started'));
