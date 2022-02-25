2957165b87a3c2a764f55e223cd40a5b979025d0
Chandramouli B <chandramoulib@hydkspl.com>
2022-02-25 18:04:55 +0530

    Update

--- server.js
+++ server.js
@@ -5,11 +5,6 @@ const port = 3000
 
 app.use(express.static(__dirname + '/dist/ecom-portal'));
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
 
 app.use('/*', (req, res) => {
   //console.log("Index Page");
